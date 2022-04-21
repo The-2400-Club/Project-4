7d940581d4d8751ab79e66c1e729869f6eda6653
Jaymez51 <98789416+Jaymez51@users.noreply.github.com>
2022-04-20 20:34:55 -0700

    Updated Final

--- HeapDriver.java
+++ HeapDriver.java
@@ -1,29 +1,46 @@
-import java.io.*;
-import java.util.*;
+import java.io.File;
+import java.util.Scanner;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 
-public class HeapDriver
+public class HeapDriver 
 {
-    public static void main(String[] args)
-    {
-        MaxHeap<int> heap = new MaxHeap<int>();
-
-        Scanner x;
-        try
-        {
-            x = new Scanner(new File("data_sorted.txt"));
-
-            while(x.hasNext())
-            {
-                System.out.println(x.next());
-            }
-
-            x.close();
-        }
-        catch(Exception e)
-        {
-            System.out.println("Could not find the file");
-        }
-
-    }
+	public static void main(String[] args) throws FileNotFoundException
+	{	
+		MaxHeap sequential = new MaxHeap(); //new heap
+	
+		File data = new File("data_sorted.txt"); //new input file 
+		
+		Scanner inputData = new Scanner(data);	//read data from input file
+		
+		int i = 0; 
+		int[] tempArray = new int[100];
+		
+		while (inputData.hasNextInt()) //loop used to store data
+		{
+			int number = Integer.parseInt(inputData.nextLine());
+			tempArray[i] = number; //data into array
+			sequential.add(number); //data sequentially added to heap
+			i++;
+		}
+		
+		inputData.close(); //file closed
+		
+		MaxHeap optimal = new MaxHeap(tempArray);	//new heap 
+		
+		PrintWriter outputFile = new PrintWriter("output.txt"); //output file created
+		
+		outputFile.println("=====================================================================");
+		outputFile.println("Heap built using sequential insertions: " + sequential.topTen() + "...");				
+		outputFile.println("Number of swaps in the heap creation: " + sequential.getSwaps());
+		outputFile.println("Heap after 10 removals: " + sequential.removeTop() + "...");
+		outputFile.println();
+		outputFile.println("Heap built using optimal method: " + optimal.topTen() + "...");
+		outputFile.println("Number of swaps in the heap creation: " + optimal.getSwaps());
+		outputFile.println("Heap after 10 removals: " + optimal.removeTop() + "...");
+		outputFile.println("=====================================================================");
+		
+		outputFile.close(); //output file closed
+	}
 
 }
--- MaxHeap.java
+++ MaxHeap.java
@@ -1,119 +1,231 @@
-public final class MaxHeap<T extends Comparable<? super T>> implements MaxHeapInterface<T>
+public class MaxHeap
 {
-    private T[] heap;       //Array of heap entries
-    private int lastIndex;  //Index of last entry
-    private static final int DEFAULT_CAPACITY = 25;
-    private int swaps;
+	private int[] heap;	//Array of heap entries
+	
+	private int lastIndex;	//Index of last entry
+	private int numberOfSwaps = 0;	//initialization of number of swaps
+	
+	private boolean initialized = false;
+	
+	private static final int DEFAULT_CAPACITY = 25;
+	private static final int MAX_CAPACITY = 10000;
 
-    public MaxHeap()
-    {
-        this(DEFAULT_CAPACITY); //call next constructor
-    } //end default constructor
+	public MaxHeap()
+	{
+		this(DEFAULT_CAPACITY); // Call next constructor
+	}	//end default constructor
+	
+	public MaxHeap(int initialCapacity)
+	{
+		//The cast is safe because the new array contains all null entries
+		if (initialCapacity < DEFAULT_CAPACITY)
+			initialCapacity = DEFAULT_CAPACITY;
+		else checkCapacity(initialCapacity);
+		
+		int[] tempHeap = new int[initialCapacity + 1];
+		heap = tempHeap;
+		lastIndex = 0;
+		initialized = true;
+	}	//end constructor
+	
+	//Method used by heap constructor.
+	private void reheap(int rootIndex)
+	{
+		boolean done = false;
+			
+		int orphan = heap[rootIndex];
+		int leftChildIndex = 2 * rootIndex;
+			
+		while (!done && (leftChildIndex <= lastIndex))
+		{
+			int largerChildIndex = leftChildIndex;
+			int rightChildIndex = leftChildIndex + 1;
+				
+			if ((rightChildIndex <= lastIndex) && (heap[rightChildIndex] > heap[largerChildIndex]))
+			{
+				largerChildIndex = rightChildIndex;
+			}	//end if
 
-    public MaxHeap(int initialCapacity)
-    {
-        //The cast is safe because the new array contains all null entries
-        @SuppressWarnings("unchecked")
-        T[] tempHeap = (T[]) new Comparable[initialCapacity + 1];
-        heap = tempHeap;
-        lastIndex = 0;
-        swaps = 0;
-    } //end constructor
+			if (orphan < heap[largerChildIndex])
+			{
+				heap[rootIndex] = heap[largerChildIndex];
+					
+				rootIndex = largerChildIndex;
+					
+				leftChildIndex = 2 * rootIndex;
+					
+				numberOfSwaps++;	//Swap counter
+			}
+			else
+				done = true;
+		}	//end while
+			
+		heap[rootIndex] = orphan;
+			
+	}	//end reheap
 
-    public void reheap(int rootIndex)
-    {
-        boolean done = false;
-        T orphan = heap[rootIndex];
-        int leftChildIndex = 2 * rootIndex;
+	// Adds to the heap and is used for sequential insertion method.
+	public void add(int newEntry)
+	{
+		checkInitialization();
+		
+		int newIndex = lastIndex + 1;
+		int parentIndex = newIndex / 2;
+		
+		while ((parentIndex > 0) && (newEntry > heap[parentIndex]))
+		{
+			heap[newIndex] = heap[parentIndex];
+			
+			newIndex = parentIndex;
+			parentIndex = newIndex / 2;
+			
+			numberOfSwaps++;	//swap counter
+		}
+		
+		heap[newIndex] = newEntry;
+		
+		lastIndex++;
+		
+		ensureCapacity();
+	}	//end add
+	
+	//Removes heap's root.
+	public int removeMax()
+	{
+		checkInitialization();
+		
+		int root = 0;
+		
+		if (!isEmpty())
+		{
+			root = heap[1];				//Return value
+			
+			heap[1] = heap[lastIndex];	//Form a semiheap
+			lastIndex--;				//Decrease size
+			
+			reheap(1);					//Transform to a heap
+		}	//end if	
+		
+		return root;
+		
+	}	//end removeMax
+		
+	//Finds the root of the heap.
+	public int getMax()
+	{
+		checkInitialization();
+		
+		int root = 0;
+		
+		if (!isEmpty())
+			root = heap[1];
+		
+		return root;
+	}	//end getMax();
+		
+	// Throws an exception if the client requests a capacity that is too large.
+	private void checkCapacity(int initialCapacity) 
+	{
+		if (initialCapacity > MAX_CAPACITY)
+			throw new IllegalStateException("Attempt to create a bag whose " + "capacity exceeds allowed " + "maximum of " + MAX_CAPACITY);
+	}	//end checkCapacity
+	
+	//Checks to make sure if the heap is at good capacity and doubles if it is not.
+	private void ensureCapacity()
+	{
+		if(lastIndex+1 >= heap.length)
+			heap = doubleCapacity();			
+	}	//end ensureCapacity
+	
+	//Doubles the capacity of the heap.
+	private int[] doubleCapacity()
+	{
+		int[] newCap = new int[heap.length * 2];
+		
+		for (int i = 0; i < heap.length; i++)
+			newCap[i] = heap[i];
+		
+		return newCap;
+	}	//end doubleCapacity
+	
+	/** Checks if this heap is empty.
+		@return True if heap is empty.*/
+	public boolean isEmpty()
+	{
+		return lastIndex < 1;
+	}	//end isEmpty
+	
+	public int getSize()
+	{
+		return lastIndex;
+	}	//end getSize;
+	
+	//Clear all entries from heap.
+	public void clear()
+	{
+		checkInitialization();
+		while (lastIndex > -1)
+		{
+			heap[lastIndex] = 0;
+			lastIndex--;
+		}	//end while
+		
+		lastIndex = 0;
+	}	//end clear
+	
+	// Throws an exception of this object is not initialized.
+	private void checkInitialization()
+	{
+		if(!initialized)
+			throw new IllegalStateException("Heap not initialized.");
+	} //end checkInitialization
+	
+	//Uses reheap method to create a heap.
+	public MaxHeap(int[] entries)
+	{
+		this(entries.length);	//Call other constructor
+		
+		//Copy given array to data field
+		for (int index = 0; index < entries.length; index++)
+		{
+			heap[index + 1] = entries[index];
+			
+			lastIndex++;
+		}		
+		//Create heap
+		for (int rootIndex = lastIndex / 2; rootIndex > 0; rootIndex--)
+		{
+			reheap(rootIndex);
+		}		
+	}	//end heap
 
-        while(!done && (leftChildIndex <= lastIndex))
-        {
-            int largerChildIndex = leftChildIndex;
-            int rightChildIndex = leftChildIndex + 1;
-
-            if((rightChildIndex <= lastIndex) &&
-                heap[rightChildIndex].compareTo(heap[largerChildIndex]) > 0)
-            {
-                largerChildIndex = rightChildIndex;
-                swaps++;
-            } //end if
-
-            if(orphan.compareTo(heap[largerChildIndex]) < 0)
-            {
-                heap[rootIndex] = heap[largerChildIndex];
-                swaps++;
-                rootIndex = largerChildIndex;
-                leftChildIndex = 2 * rootIndex;
-            }
-            else
-            {
-                done = true;
-            }
-        } //end while
-        
-        heap[rootIndex] = orphan;
-        swaps++;
-    } //end reheap
-
-    public void add(T newEntry)
-    {
-        int newIndex = lastIndex + 1;
-        int parentIndex = newIndex / 2;
-        while((parentIndex > 0) && newEntry.compareTo(heap[parentIndex]) > 0)
-        {
-            heap[newIndex] = heap[parentIndex];
-            swaps++;
-            newIndex = parentIndex;
-            parentIndex = newIndex / 2;
-        } //end while
-        heap[newIndex] = newEntry;
-        swaps++;
-        lastIndex++;
-    } //end add
-
-    public T removeMax()
-    {
-        T root = null;
-
-        if(!isEmpty())
-        {
-            root = heap[1];
-            heap[1] = heap[lastIndex];
-            lastIndex--;
-            reheap(1);
-        } //end if
-
-        return root;
-    } //end removeMax
-
-    public T getMax()
-    {
-        T root = null;
-        if(!isEmpty())
-        {
-            root = heap[1];
-        }
-        return root;
-    } //end getMax
-
-    public boolean isEmpty()
-    {
-        return lastIndex < 1;
-    } //end isEmpty
-
-    public int getSize()
-    {
-        return lastIndex;
-    } //end getSize
-
-    public void clear()
-    {
-        while(lastIndex < -1)
-        {
-            heap[lastIndex] = null;
-            lastIndex--;
-        } //end while
-        lastIndex = 0;
-    } //end clear
-
+	//Gets the first ten numbers in the heap and returns them.
+	public String topTen()
+	{
+		String result = "";
+		
+		for(int i = 1; i <= 10; i++)
+		{
+			result = result + Integer.toString(heap[i]) + ",";
+		}
+		
+		return result;
+	}	//end firstTen
+	
+	//Removes first ten roots in the heap and returns the next ten.
+	public String removeTop()
+	{
+		for(int j = 0; j < 10; j++)
+		{
+			removeMax();
+		}
+		
+		return topTen();
+	}	//end removeTen
+	
+	//Returns number of swaps in the called method.
+	public int getSwaps() 
+	{
+		return numberOfSwaps;
+	}	//end getSwaps
 }
